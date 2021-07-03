88d008ebd542cb03836aa2c7da6729d90f0f755a
Itay Malka <itaymalka@Itays-MBP.lan>
2021-07-03 14:11:37 +0300

    Update index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var calc = 2;
 app.get('/', function (req, res) {
- res.send('Welcome To The Webinar ! :-)' + "</br>" + 'The result of 1+1 is: ' + calc);
+ res.send('Welcome To The Webinar !' + "</br>" + 'The result of 1+1 is: ' + calc);
 });
 app.listen(process.env.PORT || 3000);
  
