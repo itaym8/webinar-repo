3c2be72df7fca2d64eef030b5fefbd4b20cf2cb1
Itay Malka <itaymalka@Itays-MBP.lan>
2021-07-04 14:18:48 +0300

    Update index.js

--- test/test.js
+++ test/test.js
@@ -1,11 +1,3 @@
-//var assert = require('assert');
-//var request = require('supertest');
-//var app = require('../index.js');
-//describe('GET /', function() {
-// it('respond with Welcome To The Webinar ! :-)', function(done) {
-// request(app).get('/').expect('Welcome To The Webinar ! :-)', done);
-// });
-//});
 const assert = require('assert');
 describe('Simple Math Test', () => {
  it('should return 2', () => {
