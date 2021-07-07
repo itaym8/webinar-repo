ce5cd2554a306cd61c9d7f73b28237556a9d65e9
Itay Malka <itaymalka@Itays-MBP.lan>
2021-07-07 20:22:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var calc = 2;
 app.get('/', function (req, res) {
- res.send('Welcome To The Webinar !' + "</br>" + 'The result of 1+1 is: ' + calc);
+ res.send('Welcome To The Webinar, Great People ! And Thanks for taking part in this video.' + "</br>" + 'The result of 1+1 is: ' + calc);
 });
 app.listen(process.env.PORT || 3000);
  
